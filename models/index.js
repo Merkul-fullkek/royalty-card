33c45989d8d02a69b9a13e022cfac64633c1492e
Merkull-fullkek <ofmencall@gmail.com>
2022-09-07 16:31:14 +0300

    relation

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const sequelize = require('../db');
+const User = require ('./user.model');
+const Card = require ('./card.model');
+const Owner = require('./owner.model')
+const Place = require('./place.model');
+
+User.hasMany(Card)
+Card.belongsTo(User)
+
+Place.hasMany(Card)
+Card.belongsTo(Place)
+
+Place.hasOne(Owner)
+Owner.belongsTo(Place)
+
+module.exports = {
+    User,
+    Place,
+    Card,
+    Owner
+}
