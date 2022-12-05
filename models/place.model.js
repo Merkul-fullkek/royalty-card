68f8629930d006729c06f261285a2ea5bae0588e
Lentyai <84227189+Merkul-fullkek@users.noreply.github.com>
2022-12-05 22:06:35 +0300

    Delete models directory

--- models/place.model.js
+++
@@ -1,8 +0,0 @@
-const {DataTypes} = require('sequelize');
-const sequelize = require('../db');
-const Place = sequelize.define('place', {
-  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
-  name: {type: DataTypes.STRING},
-  discription: {type: DataTypes.TEXT},
-});
-module.exports = Place;
