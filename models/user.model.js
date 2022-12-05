efe65fa689511bf24b13ca334ccd82bda5db7800
Lentyai <84227189+Merkul-fullkek@users.noreply.github.com>
2022-12-05 22:29:30 +0300

    Delete models directory

--- models/user.model.js
+++
@@ -1,9 +0,0 @@
-const {DataTypes} = require('sequelize');
-const sequelize = require('../db');
-const User = sequelize.define('user', {
-  id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
-  email: {type: DataTypes.STRING, unique: true},
-  password: {type: DataTypes.STRING},
-  username: {type: DataTypes.STRING},
-});
-module.exports= User;
